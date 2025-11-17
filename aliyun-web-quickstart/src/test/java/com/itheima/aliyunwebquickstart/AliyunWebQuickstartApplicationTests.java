d7c3d825a063ea5bbbce466b54a33c9802cbb50e
ZhangYanChong <zhangyanchong@gdhdkj.com>
2025-11-17 15:31:11 +0800

    ✨ Feat: 官方脚手架与阿里云脚手架创建springboot-web 编写简单的hello控制器

---
+++ aliyun-web-quickstart/src/test/java/com/itheima/aliyunwebquickstart/AliyunWebQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itheima.aliyunwebquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AliyunWebQuickstartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
