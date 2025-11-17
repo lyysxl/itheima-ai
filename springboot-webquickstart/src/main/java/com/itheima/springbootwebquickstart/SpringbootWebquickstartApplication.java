b1f1bb06b8a6d01069459d3b9153a7f4baa4042d
ZhangYanChong <zhangyanchong@gdhdkj.com>
2025-11-17 15:06:29 +0800

    ✨ Feat: springboot-web 快速体验

---
+++ springboot-webquickstart/src/main/java/com/itheima/springbootwebquickstart/SpringbootWebquickstartApplication.java
@@ -0,0 +1,13 @@
+package com.itheima.springbootwebquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebquickstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebquickstartApplication.class, args);
+    }
+
+}
