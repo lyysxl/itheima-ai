3ee13635ab758f973749bc379a6272467506bb4a
ZhangYanChong <zhangyanchong@gdhdkj.com>
2025-11-18 14:02:30 +0800

    ✨ Feat: 使用spring官方脚手架与mybatis实现CRUD

--- springboot-mybatis-quickstart/src/test/java/com/itheima/springbootmybatisquickstart/SpringbootMybatisQuickstartApplicationTests.java
+++ springboot-mybatis-quickstart/src/test/java/com/itheima/springbootmybatisquickstart/SpringbootMybatisQuickstartApplicationTests.java
@@ -2,6 +2,8 @@ package com.itheima.springbootmybatisquickstart;
 
 import com.itheima.springbootmybatisquickstart.mapper.UserMapper;
 import com.itheima.springbootmybatisquickstart.pojo.User;
+import com.mysql.cj.callback.MysqlCallbackHandler;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,4 +21,38 @@ class SpringbootMybatisQuickstartApplicationTests {
         users.forEach(System.out::println);
     }
 
+    @Test
+    public void testDeleteUserById() {
+        Integer count = userMapper.deleteById(5);
+        System.out.println("count = " + count);
+    }
+
+    @Test
+    public void testInsertUser() {
+        User user = new User();
+        user.setUsername("admin");
+        user.setPassword("<PASSWORD>");
+        user.setName("管理员");
+        user.setAge(18);
+        Integer count = userMapper.insert(user);
+        System.out.println("count = " + count);
+    }
+
+    @Test
+    public void testUpdateUser() {
+        User user = new User();
+        user.setId(6);
+        user.setUsername("admin222");
+        user.setPassword("qwe123");
+        user.setName("管理员2222");
+        user.setAge(18);
+        Integer count = userMapper.update(user);
+        System.out.println("count = " + count);
+    }
+
+    @Test
+    public void testFindByUserAndPassword() {
+        User user = userMapper.findByUserAndPassword("daqiao", "123456");
+        System.out.println("user = " + user);
+    }
 }
